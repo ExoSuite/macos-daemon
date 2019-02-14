0db02128067b732743b95a4be52526af3ab4a03f
Loïc Lopez <loic.lopez@epitech.eu>
2019-02-14 14:40:40 -0500

    add bamboo agent daemon

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ $(ps -ef | grep -v grep | grep atlassian-bamboo&&gent | wc -l) -le 0 ]]
+then
+
+# start bamboo agent
+java -jar /Users/exosuite/atlassian-bamboo-agent-installer.jar https://bamboo.dev.exosuite.fr/agentServer/
+echo "atlassian-bamboo-agent-installer.jar Agent Started"
+
+else
+ echo "atlassian-bamboo-agent-installer.jar Agent already Started"
+fi
