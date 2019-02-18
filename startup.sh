774bb06216933b4a9819d1b2ae35ddcd378faac8
Loïc Lopez <loic.lopez@epitech.eu>
2019-02-18 09:30:31 -0500

    fix agent

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ if [[ $(ps -ef | grep -v grep | grep atlassian-bamboo-agent | wc -l) -le 0 ]]
 then
 
 # start bamboo agent
-java -jar /Users/exosuite/atlassian-bamboo-agent-installer.jar https://bamboo.dev.exosuite.fr/agentServer/
+java -jar /Users/exosuite/atlassian-bamboo-agent-installer.jar https://bamboo.teamexosuite.cloud/agentServer/
 echo "atlassian-bamboo-agent-installer.jar Agent Started"
 
 else
